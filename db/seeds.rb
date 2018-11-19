15b96a50f3fa8d12079af7df83154e48df007713
Dang-Thanh Tran <trandgthanh@gmail.com>
2018-11-19 21:16:43 +0100

    Finished exercise

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+puts 'Creating 10 fake articles...'
+10.times do
+  article = Article.new(
+    title: Faker::StrangerThings.character,
+    content: Faker::StrangerThings.quote
+  )
+  article.save!
+end
+puts 'Finished!'
